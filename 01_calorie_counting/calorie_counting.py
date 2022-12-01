6b80c20d8e29f56305c473f225054875a1f4a51a
chelmerd <chelmerd@student.42wolfsburg.de>
2022-12-01 13:30:03 +0100

    add 01 calorie counting part 2

--- 01_calorie_counting/calorie_counting.py
+++ 01_calorie_counting/calorie_counting.py
@@ -1,6 +1,7 @@
 file = open("input", "r")
 lines = file.readlines()
 
+# Part 1 - find largest sum
 sums = []
 calories = 0
 for line in lines:
@@ -9,5 +10,8 @@ for line in lines:
 		calories = 0
 	else:
 		calories += int(line)
-
 print(max(sums))
+
+# Part 2 - find sum of the three largest sum
+sums.sort(reverse=True)
+print(sum(sums[:3]))
