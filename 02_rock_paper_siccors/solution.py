af60910e6f932864edadf8267fe3c2420710c9ac
chelmerd <chelmerd@student.42wolfsburg.de>
2022-12-02 13:51:04 +0100

    refactor rock paper siccors part 1

--- 02_rock_paper_siccors/solution.py
+++ 02_rock_paper_siccors/solution.py
@@ -4,44 +4,51 @@ lines = open("input", "r").read()[:-1].split("\n")
 # B, Y => Paper
 # C, Z => Siccors
 
-
 def shape_score(shape):
-	if shape == "X":
+	if shape == "A":
 		return 1
-	elif shape == "Y":
+	elif shape == "B":
 		return 2
-	elif shape == "Z":
+	elif shape == "C":
 		return 3
 	else:
 		raise ValueError
 
-def outcome_score(opponent, player):
-	if opponent == "A":
-		if player == "Z":
-			return 0
-		elif player == "Y":
-			return 6
-		else:
-			return 3
-	elif opponent == "B":
-		if player == "X":
-			return 0
-		elif player == "Z":
-			return 6
-		else:
-			return 3
-	elif opponent == "C":
-		if player == "Y":
-			return 0
-		elif player == "X":
-			return 6
-		else:
-			return 3
-	else:
+def outcome_score(win):
+	if win == 1:
+		return 6
+	elif win == 0:
+		return 3
+	elif win == -1:
 		return 0
+
+def is_winning(opponent, player):
+	if opponent == player:
+		return 0
+	if opponent == "A" and player == "B":
+		return 1
+	elif opponent == "A" and player == "C":
+		return -1
+	elif opponent == "B" and player == "A":
+		return -1
+	elif opponent == "B" and player == "C":
+		return 1
+	elif opponent == "C" and player == "A":
+		return 1
+	elif opponent == "C" and player == "B":
+		return -1
+
+
+def translate_shape(player):
+	if player == "X":
+		return "A"
+	elif player == "Y":
+		return "B"
+	elif player == "Z":
+		return "C"
 
 score = 0
 for line in lines:
-	score += shape_score(line[2]) + outcome_score(line[0], line[2])
+	score += shape_score(translate_shape(line[2])) + outcome_score(is_winning(line[0], translate_shape(line[2])))
 
 print(score)
